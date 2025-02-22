e9715f65f86cc026111708f3ff0aaec67706c98a
leminlimez <59540996+leminlimez@users.noreply.github.com>
2025-02-21 22:25:59 -0500

    add ui photo picker

--- Da Moon/Views/ContentView.swift
+++ Da Moon/Views/ContentView.swift
@@ -6,11 +6,15 @@
 //
 
 import SwiftUI
+import PhotosUI
 
 struct ContentView: View {
     // Camera Variables
     @State private var capturedImage: UIImage? = nil
+    @State private var chosenPhotoItem: PhotosPickerItem? = nil
+    
     @State private var showCamera: Bool = false
+    @State private var showPhotoLibrary: Bool = false
     
     var body: some View {
         VStack {
@@ -18,6 +22,27 @@ struct ContentView: View {
                 showCamera.toggle()
             }) {
                 Text("Test camera")
+            }
+            Button(action: {
+                showPhotoLibrary.toggle()
+            }) {
+                Text("Test photo library")
+            }
+            if let img = capturedImage {
+                Image(uiImage: img)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(maxHeight: 500)
+            }
+        }
+        .photosPicker(isPresented: $showPhotoLibrary, selection: $chosenPhotoItem)
+        .onChange(of: chosenPhotoItem, initial: false) {
+            if let chosenPhoto = chosenPhotoItem {
+                Task {
+                    guard let imageData = try await chosenPhoto.loadTransferable(type: Data.self) else { return }
+                    guard let inputImage = UIImage(data: imageData) else { return }
+                    capturedImage = inputImage
+                }
             }
         }
         .sheet(isPresented: $showCamera) {
