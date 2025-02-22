e8e253dd762934b844f2c68dbc0adeadb9c8c91f
leminlimez <59540996+leminlimez@users.noreply.github.com>
2025-02-22 10:17:48 -0500

    change to using mijick camera library

---
+++ Da Moon/Views/MainCameraView.swift
@@ -0,0 +1,35 @@
+//
+//  MainCameraView.swift
+//  Da Moon
+//
+//  Created by lemin on 2/21/25.
+//
+
+import SwiftUI
+import MijickCamera
+
+struct MainCameraView: View {
+    @Environment(\.dismiss) var dismiss
+    @Binding var image: UIImage?
+    
+    var body: some View {
+        MCamera()
+            .setCameraScreen {
+                DefaultCameraScreen(cameraManager: $0, namespace: $1, closeMCameraAction: $2)
+                    .cameraOutputSwitchAllowed(false)
+            }
+            .onImageCaptured { capturedImage, controller in
+                image = capturedImage
+                controller.closeMCamera()
+                dismiss()
+            }
+            .setCloseMCameraAction {
+                dismiss()
+            }
+            .setCameraOutputType(.photo)
+            .setAudioAvailability(false)
+            .setGridVisibility(false)
+            .setGridVisibility(false)
+            .startSession()
+    }
+}
